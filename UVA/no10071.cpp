a82ca954600efe690e1e4fc9dffae6c1880cc962
vinhhth <vinhha2@illinois.edu>
2014-07-10 22:21:33 -0500

    lol

---
+++ UVA/no10071.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n, m;
+    while(scanf("%d %d", &n, &m) != EOF)
+    {
+    	cout << 2 * n *m << endl;
+    }
+}
